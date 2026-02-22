6dd9db51d032573d09671c3625794dad81d41395
Rakesh Tamira <rakeshreddy9narayan@gmail.com>
2026-02-22 16:44:25 -0500

    Create first api project in Console apps

---
+++ FirstAPI/Controllers/booksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using FirstAPI.models;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class booksController : Controller
+    {
+        static private List<Book> books = new List<Book>
+        {
+             new Book
+             { Id = 1,
+               Title = "Fountain Head",
+               Author = "Ayn Rand",
+               YearPublished = "1958"
+             },
+             new Book
+             { Id = 2,
+               Title = "Thus Spake Zarathusthra",
+               Author = "Friedrick Nietzsche",
+               YearPublished = "1960"
+             }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooksbyId(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+    }   
+}
---
+++ FirstAPI/models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstAPI.models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null;
+        public String Author { get; set; } = null;
+        public string YearPublished { get; set; } = null;
+    }
+}
